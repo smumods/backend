574a5fb46ac771d69351e3faa868873f2ba7701a
Gabriel <zhchuan.2016@gmail.com>
2019-06-20 02:31:18 +0800

    Add logout functionality

---
+++ app/graphql/mutations/users/log_out_user.rb
@@ -0,0 +1,21 @@
+module Mutations
+    module Users
+        class LogOutUser < Mutations::BaseMutation
+            null true
+
+            argument :token, String, required: true
+
+            field :is_logged_out, String, null: true
+
+            def resolve(token: nil)
+                binding.pry
+                return unless token
+                if context[:session][:token] == token
+                    context[:session][:token] = nil
+                    return { is_logged_out: true }
+                end
+                return { is_logged_out: false }
+            end
+        end
+    end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,5 +4,6 @@ module Types
     field :create_user, mutation: Mutations::Users::CreateUser
     field :create_temporary_user, mutation: Mutations::Users::CreateTemporaryUser
     field :login_user, mutation: Mutations::Users::LogInUser
+    field :logout_user, mutation: Mutations::Users::LogOutUser
   end
 end
