5df838bbb72225e735acd2b6449d054ee654cc6a
Gabriel <zhchuan.2016@gmail.com>
2019-06-20 00:53:46 +0800

    Add user login mutation

---
+++ app/graphql/mutations/users/log_in_user.rb
@@ -0,0 +1,27 @@
+module Mutations
+    module Users
+        class LogInUser < Mutations::BaseMutation
+
+            argument :email, String, required: true
+            argument :password, String, required: true
+
+            field :token, String, null: true
+            field :user, Types::UserType, null: true
+            
+
+            def resolve(email: nil, password: nil)
+                return unless email
+                begin
+                    user = User.find_by_email(email)
+                    return unless user
+                    return unless user.valid_password?(password) 
+                    crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
+                    token = crypt.encrypt_and_sign("user-id:#{ user.id }")
+                    { user: user, token: token }
+                rescue ActiveRecord::RecordInvalid => invalid
+                    GraphQL::ExecutionError.new("Invalid Attributes for #{invalid.record.class.name}: #{invalid.record.errors.full_messages.join(', ')}")
+                end
+            end
+        end
+    end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,6 @@ module Types
     field :create_link, mutation: Mutations::CreateLink
     field :create_user, mutation: Mutations::Users::CreateUser
     field :create_temporary_user, mutation: Mutations::Users::CreateTemporaryUser
+    field :login_user, mutation: Mutations::Users::LogInUser
   end
 end
