40ee85973c977ea53dbd10325a16c80da1c58200
Gabriel <zhchuan.2016@gmail.com>
2019-06-20 16:45:48 +0800

    Update seeds to create a Professor record for each found in seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,17 +38,21 @@ if (Course.all.length == 0)
             courses_data = JSON.parse(term_file_contents)
             courses_data.keys.each do |term|
                 courses_data[term].keys.each do |module_code|
+                    courses_info_data = courses_data[term][module_code]["data"]
+                    courses_info_data["info_instructors"].each do |instructor_name|
+                        Professor.find_or_create_by(name: instructor_name.strip.squeeze(' '))
+                    end
                     Course.create!({
                         module_code: module_code,
                         name: courses_data[term][module_code]["name"],
-                        career: courses_data[term][module_code]["data"]["info_career"],
-                        grading_basis: courses_data[term][module_code]["data"]["info_grading_basis"],
-                        course_components: courses_data[term][module_code]["data"]["info_course_components"],
-                        campus: courses_data[term][module_code]["data"]["info_campus"],
-                        academic_group: courses_data[term][module_code]["data"]["info_academic_group"],
-                        academic_organization: courses_data[term][module_code]["data"]["info_academic_organization"],
-                        credit_units: courses_data[term][module_code]["data"]["info_units"],
-                        description: courses_data[term][module_code]["data"]["info_description"],
+                        career: courses_info_data["info_career"],
+                        grading_basis: courses_info_data["info_grading_basis"],
+                        course_components: courses_info_data["info_course_components"],
+                        campus: courses_info_data["info_campus"],
+                        academic_group: courses_info_data["info_academic_group"],
+                        academic_organization: courses_info_data["info_academic_organization"],
+                        credit_units: courses_info_data["info_units"],
+                        description: courses_info_data["info_description"],
                         term: term
                     })
                 end
