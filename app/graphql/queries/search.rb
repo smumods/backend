5bc87686cbd7768de2f3d7ab5ff867a4cdc9b8da
Gabriel <zhchuan.2016@gmail.com>
2019-06-22 15:49:28 +0800

    Modify graphql search to search all Professors and Courses

--- app/graphql/queries/search.rb
+++ app/graphql/queries/search.rb
@@ -7,10 +7,29 @@ module Queries
         type [Types::SearchResultType], null: true
 
         def resolve(query_string:)
+            return if query_string.empty?
+            
             results = []
-            results = results + Course.where("name ilike ?", "%#{query_string}%")
-            results = results + Professor.where("name ilike ?", "%#{query_string}%")
-            results
+            professors = Set.new
+            courses = Set.new
+            # Search by course, then pull the relevant professors
+            # Get the courses first
+            course_courses = Course.where("name ilike ?", "%#{query_string}%") # by name
+            course_courses = course_courses + Course.where("module_code ilike ?", "%#{query_string}%") # by module_code
+            course_courses.each { |course| courses.add(course) }
+            # Get the professors next
+            courses.collect(&:professors).flatten.each { |professor| professors.add(professor) }
+
+            # Search by professor, then pull the relevant courses
+            professor_professors = Professor.where("name ilike ?", "%#{query_string}%")
+            professor_professors.each { |professor| professors.add(professor) }
+            professor_professors.collect(&:courses).flatten.each { |course| courses.add(course) }
+            
+            # Get all professors teaching each course
+            courses.each { |course| course.professors.each { |professor| professors.add(professor) } }
+
+            # Return a merged array
+            professors.to_a + courses.to_a
         end
     end
 end
