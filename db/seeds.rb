555d8e7cf7d94d279e56b712c0a57a0358071b7e
Gabriel <zhchuan.2016@gmail.com>
2019-06-22 23:24:22 +0800

    Add more term data for seeding and fix seeds exception for duplicate module + term

--- db/seeds.rb
+++ db/seeds.rb
@@ -42,19 +42,22 @@ if (Course.count == 0)
                     courses_info_data["info_instructors"].each do |instructor_name|
                         Professor.find_or_create_by(name: instructor_name.strip.squeeze(' '))
                     end
-                    Course.create!({
-                        module_code: module_code,
-                        name: courses_data[term][module_code]["name"],
-                        career: courses_info_data["info_career"],
-                        grading_basis: courses_info_data["info_grading_basis"],
-                        course_components: courses_info_data["info_course_components"],
-                        campus: courses_info_data["info_campus"],
-                        academic_group: courses_info_data["info_academic_group"],
-                        academic_organization: courses_info_data["info_academic_organization"],
-                        credit_units: courses_info_data["info_units"],
-                        description: courses_info_data["info_description"],
-                        term: term
-                    })
+                    begin
+                        Course.create!({
+                            module_code: module_code,
+                            name: courses_data[term][module_code]["name"],
+                            career: courses_info_data["info_career"],
+                            grading_basis: courses_info_data["info_grading_basis"],
+                            course_components: courses_info_data["info_course_components"],
+                            campus: courses_info_data["info_campus"],
+                            academic_group: courses_info_data["info_academic_group"],
+                            academic_organization: courses_info_data["info_academic_organization"],
+                            credit_units: courses_info_data["info_units"],
+                            description: courses_info_data["info_description"],
+                            term: term
+                        })
+                    rescue Exception => e
+                    end
                     
                 end
             end
@@ -114,21 +117,6 @@ if (Review.count == 0)
     end
 end
 
-course = Course.create!({
-    name: "Independent Study",
-    career: "Graduate - PhD Business (FNCE)",
-    grading_basis: "Graded",
-    course_components: "Independent Study Required",
-    campus: "Main Campus",
-    academic_group: "Lee Kong Chian Sch of Business",
-    academic_organization: "LKCSB GPO",
-    module_code: "IDST704",
-    credit_units: 1.0,
-    description:
-    "Each independent study is a one-on-one research tutorial between the student and a selected faculty, on a topic mutually agreed upon by the student and the faculty. A research term paper is required in each independent study.",
-    term: "2018-19 Term 2",
-})
-
 Review.create!(
     module_review: Faker::Lorem.paragraph,
     is_anonymous: [true, false][rand(2)],
@@ -140,5 +128,5 @@ Review.create!(
     module_review: Faker::Lorem.paragraph,
     is_anonymous: [true, false][rand(2)],
     user: User.find(User.all.ids[rand(User.all.ids.count)]),
-    course: course
+    course: Course.where(module_code: "IDST704", term: "2018-19 Term 2").first
 )
