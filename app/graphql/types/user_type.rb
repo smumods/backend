a1325cf40c323de221b7de12941339b490031617
Gabriel <zhchuan.2016@gmail.com>
2019-06-25 18:15:03 +0800

    Add feature for user to get all votes and reviews via all_votes and all_reviews in UserType.

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -5,9 +5,19 @@ module Types
     field :lastName, String, null: true
     field :books, [Types::BookType], null: true
     field :books_count, Integer, null: true
+    field :all_votes, [Types::VoteType], null: true
+    field :all_reviews, [Types::ReviewType], null: true
 
     def books_count
       books.size
+    end
+
+    def all_votes
+      self.object.votes
+    end
+
+    def all_reviews
+      self.object.reviews
     end
   end
 end
--- app/graphql/types/vote_type.rb
+++ app/graphql/types/vote_type.rb
@@ -1,9 +1,10 @@
 module Types
     class VoteType < Types::BaseObject
+        field :id, ID, null: false
         field :vote_type, Int, null: false
         field :review_type, String, null: false
-        field :user_id, Types::UserType, null: false
-        field :review_id, Types::ReviewType, null: false
+        field :user, Types::UserType, null: false
+        field :review, Types::ReviewType, null: false
         field :created_at, Types::DateTimeType, null: false
         field :updated_at, Types::DateTimeType, null: false
     end
