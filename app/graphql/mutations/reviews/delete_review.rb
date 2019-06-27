15fcb58c092291f343beb6174aae99a815ab9c24
Gabriel <zhchuan.2016@gmail.com>
2019-06-28 00:07:59 +0800

    Feature: Add user field to deleteReview

--- app/graphql/mutations/reviews/delete_review.rb
+++ app/graphql/mutations/reviews/delete_review.rb
@@ -8,6 +8,7 @@ module Mutations
             # But specify field types instead
             field :success, Boolean, null: true
             field :message, String, null: true
+            field :user, Types::UserType, null: true
 
             # Arguments
             argument :id, Int, required: true
@@ -19,10 +20,18 @@ module Mutations
                     raise GraphQL::ExecutionError.new("Authentication required")
                 end
                 review = Review.where(user: current_user, id: id).first
-                return { success: false, message: "You either do not have the rights to remove the review or there is no such review found" } if review.nil?
-                review.delete
-                return { success: true, message: "You have successfully deleted your review!" } if review.destroyed?
-                return { success: false, message: "An unexpected error has occurred while trying to delete the review!"}
+                if review.nil?
+                    raise GraphQL::ExecutionError.new("Caught ya! You are trying to delete an invalid review!")
+                end
+                if review.delete
+                    return {
+                        user: current_user,
+                        message: "You have successfully deleted your review!",
+                        success: true
+                    }
+                else
+                    raise GraphQL::ExecutionError.new("You can't delete this due to an error! Invalid review or you do not have the rights to delete?")
+                end
             end
         end
     end
