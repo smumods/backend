30b3de9009c52ba9991163f34ed6c092c4687312
Gabriel <zhchuan.2016@gmail.com>
2019-06-28 04:40:45 +0800

    Update seeds file and remove .DS_Store which causes errors with seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,32 +34,35 @@ if (Course.count == 0)
     term_files = Dir.entries(data_directory_location).select { |e| !(e == '.' || e == '..') }
     term_files.each do |term_file_name|
         File.open("#{data_directory_location}/#{term_file_name}") do |term_file|
-            term_file_contents = term_file.read
-            courses_data = JSON.parse(term_file_contents)
-            courses_data.keys.each do |term|
-                courses_data[term].keys.each do |module_code|
-                    courses_info_data = courses_data[term][module_code]["data"]
-                    courses_info_data["info_instructors"].each do |instructor_name|
-                        Professor.find_or_create_by(name: instructor_name.strip.squeeze(' ')) if instructor_name != "Staff"
-                    end
-                    begin
-                        Course.find_or_create_by!(module_code: module_code, term: term) do |c|
-                            c.name = courses_data[term][module_code]["name"]
-                            c.career = courses_info_data["info_career"]
-                            c.grading_basis = courses_info_data["info_grading_basis"]
-                            c.course_components = courses_info_data["info_course_components"]
-                            c.campus = courses_info_data["info_campus"]
-                            c.academic_group = courses_info_data["info_academic_group"]
-                            c.academic_organization = courses_info_data["info_academic_organization"]
-                            c.credit_units = courses_info_data["info_units"]
-                            c.description = courses_info_data["info_description"]
-                            c.academic_group_abbreviation = Course.academic_group_abbreviation(courses_info_data["info_academic_group"])
-                            c.module_code_front = Course.module_code_front(module_code)
+            begin
+                term_file_contents = term_file.read
+                courses_data = JSON.parse(term_file_contents)
+                courses_data.keys.each do |term|
+                    courses_data[term].keys.each do |module_code|
+                        courses_info_data = courses_data[term][module_code]["data"]
+                        courses_info_data["info_instructors"].each do |instructor_name|
+                            Professor.find_or_create_by(name: instructor_name.strip.squeeze(' ')) if instructor_name != "Staff"
                         end
-                    rescue Exception => e
+                        begin
+                            Course.find_or_create_by!(module_code: module_code, term: term) do |c|
+                                c.name = courses_data[term][module_code]["name"]
+                                c.career = courses_info_data["info_career"]
+                                c.grading_basis = courses_info_data["info_grading_basis"]
+                                c.course_components = courses_info_data["info_course_components"]
+                                c.campus = courses_info_data["info_campus"]
+                                c.academic_group = courses_info_data["info_academic_group"]
+                                c.academic_organization = courses_info_data["info_academic_organization"]
+                                c.credit_units = courses_info_data["info_units"]
+                                c.description = courses_info_data["info_description"]
+                                c.academic_group_abbreviation = Course.academic_group_abbreviation(courses_info_data["info_academic_group"])
+                                c.module_code_front = Course.module_code_front(module_code)
+                            end
+                        rescue Exception => e
+                        end
+                        
                     end
-                    
                 end
+            rescue Exception => e
             end
         end
     end
@@ -70,24 +73,27 @@ if (ProfessorCourse.count == 0)
     data_directory_location = "db/seeds/data"
     term_files = Dir.entries(data_directory_location).select { |e| !(e == '.' || e == '..') }
     term_files.each do |term_file_name|
-        File.open("#{data_directory_location}/#{term_file_name}") do |term_file|
-            term_file_contents = term_file.read
-            courses_data = JSON.parse(term_file_contents)
-            courses_data.keys.each do |term|
-                courses_data[term].keys.each do |module_code|
-                    courses_info_data = courses_data[term][module_code]["data"]
-                    course = Course.where(module_code: module_code, term: term).first
-                    courses_info_data["info_instructors"].each do |instructor_name|
-                        begin
-                            if instructor_name != "Staff"
-                                professor = Professor.find_by_name(instructor_name)
-                                ProfessorCourse.find_or_create_by!(course: course, professor: professor)
+        begin
+            File.open("#{data_directory_location}/#{term_file_name}") do |term_file|
+                term_file_contents = term_file.read
+                courses_data = JSON.parse(term_file_contents)
+                courses_data.keys.each do |term|
+                    courses_data[term].keys.each do |module_code|
+                        courses_info_data = courses_data[term][module_code]["data"]
+                        course = Course.where(module_code: module_code, term: term).first
+                        courses_info_data["info_instructors"].each do |instructor_name|
+                            begin
+                                if instructor_name != "Staff"
+                                    professor = Professor.find_by_name(instructor_name)
+                                    ProfessorCourse.find_or_create_by!(course: course, professor: professor)
+                                end
+                            rescue Exception => e
                             end
-                        rescue Exception => e
                         end
                     end
                 end
             end
+        rescue Exception => e
         end
     end
 end
