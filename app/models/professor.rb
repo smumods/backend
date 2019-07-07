6af3e210a770189b976a57319328cc48a507281c
Gabriel <zhchuan.2016@gmail.com>
2019-07-07 20:58:49 +0800

    Add inverse_of relationship for Professor and ProfessorCourse

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -4,9 +4,9 @@ class Professor < ApplicationRecord
     friendly_id :name, use: :slugged
 
     # Relationships
-    has_many :professor_courses
+    has_many :professor_courses, inverse_of: :professors
     has_many :courses, through: :professor_courses
-    has_many :reviews
+    has_many :reviews, inverse_of: :reviews
 
     # Bookmarks/Likes/Etc
     # has_many :like_by_users
