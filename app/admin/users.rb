825fab6466fe3de4373804c2cb73837c29efe14f
Gabriel <zhchuan.2016@gmail.com>
2019-07-18 16:20:08 +0800

    Update ActiveAdmin for users to allow zach to edit verified in staging env

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,38 +1,57 @@
 ActiveAdmin.register User do
+  if Rails.env.staging? or Rails.env.development?
+    permit_params :password_reset_tries_count, :password_token_tries_count, :verified
+  else
     permit_params :password_reset_tries_count, :password_token_tries_count
-  
-    index do
-      selectable_column
-      id_column
-      column :email
-      column :first_name
-      column :last_name
-      column :verified
-      column :password_reset_tries_count
-      column :password_token_tries_count
-      column :created_at
-      column :updated_at
-      actions
-    end
-  
-    filter :email
-    filter :password_reset_tries_count
-    filter :password_token_tries_count
-    filter :verified
-    filter :created_at
-    filter :updated_at
-  
-    form do |f|
-      f.inputs do
-        f.input :email, as: :readonly
-        f.input :first_name, as: :readonly
-        f.input :last_name, as: :readonly
-        f.input :verified, as: :readonly
-        f.input :password_reset_tries_count
-        f.input :password_token_tries_count
-      end
-      f.actions
-    end
-  
   end
-  
+  index do
+    selectable_column
+    id_column
+    column :email
+    column :first_name
+    column :last_name
+    column :verified
+    column :password_reset_tries_count
+    column :password_token_tries_count
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  filter :email
+  filter :password_reset_tries_count
+  filter :password_token_tries_count
+  filter :verified
+  filter :created_at
+  filter :updated_at
+
+  form do |f|
+    f.inputs do
+      f.input :email, as: :readonly
+      f.input :first_name, as: :readonly
+      f.input :last_name, as: :readonly
+      if Rails.env.staging? or Rails.env.development?
+        f.input :verified
+      else
+        f.input :verified, as: :readonly
+      end
+      f.input :password_reset_tries_count
+      f.input :password_token_tries_count
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :email
+      row :first_name
+      row :last_name
+      row :verified
+      row :password_reset_tries_count
+      row :password_token_tries_count
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
+end
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
     # config.assets.css_compressor = :sass
   
     # Do not fallback to assets pipeline if a precompiled asset is missed.
-    config.assets.compile = false
+    config.assets.compile = true
   
     # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
   
@@ -57,7 +57,7 @@ Rails.application.configure do
     config.log_tags = [ :request_id ]
   
     # Use a different cache store in production.
-    # config.cache_store = :mem_cache_store
+    config.cache_store = :memory_store
   
     # Use a real queuing backend for Active Job (and separate queues per environment)
     # config.active_job.queue_adapter     = :resque
