5481a29378aa7963d5532ee882dae61b056f70c5
Gabriel <zhchuan.2016@gmail.com>
2019-07-30 19:33:58 +0800

    Add clubs feature without user auth

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,8 +1,17 @@
-class ClubsController < InheritedResources::Base
-  private
+class ClubsController < ApplicationController
 
-    def club_params
-      params.require(:club).permit(:name, :slug, :display_picture, :gallery, :description, :social_media)
-    end
+  def index
+    # Should display all the clubs that the user manages
+    @clubs = Club.all
+  end
+
+  def show
+    
+  end
+
+  private
+  def club_params
+    params.require(:club).permit(:name, :slug, :display_picture, :gallery, :description, :social_media)
+  end
 
 end
--- app/views/clubs/_club.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! club, :id, :name, :slug, :display_picture, :gallery, :description, :social_media, :created_at, :updated_at
-json.url club_url(club, format: :json)
