67d06a02b0705f3626881c0a107d2f97da6bd6db
Gabriel <zhchuan.2016@gmail.com>
2019-07-31 15:13:02 +0800

    Add slug candidates for Club

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,3 +1,21 @@
 class Club < ApplicationRecord
+    # FriendlyId
+    extend FriendlyId
+    friendly_id :slug_candidates, use: :slugged
+
+    # Validations
+
+    # Relationships
     has_one :club_admin
+
+    def slug_candidates
+        [:name, :name_and_sequence]
+    end
+
+    # Might be redundant since we have constraints on name itself (cant be duplicate)
+    def name_and_sequence
+        slug = name.to_param
+        sequence = self.class.where("slug like '#{slug}-%'").count + 2
+        "#{slug}-#{sequence}"
+    end
 end
