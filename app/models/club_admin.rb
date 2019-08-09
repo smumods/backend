c8ed0d21800a447f69eb9bf4c9219bd47e54e9fb
Gabriel <zhchuan.2016@gmail.com>
2019-08-10 02:22:31 +0800

    Update devise layout for forget password. Remove registerable option. Update Events#index

--- app/models/club_admin.rb
+++ app/models/club_admin.rb
@@ -1,8 +1,9 @@
 class ClubAdmin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
+         # registerable
 
   has_one :club
 end
