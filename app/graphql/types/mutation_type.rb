90f15f86374e6fdd792903ed6bd2294e0f300b05
Gabriel <zhchuan.2016@gmail.com>
2019-08-13 19:24:24 +0800

    User can now register for events

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -12,5 +12,6 @@ module Types
     field :edit_review, mutation: Mutations::Reviews::EditReview
     field :delete_review, mutation: Mutations::Reviews::DeleteReview
     field :bookmark, mutation: Mutations::Bookmarks::Bookmark
+    field :register_event, mutation: Mutations::Events::RegisterEvent
   end
 end
