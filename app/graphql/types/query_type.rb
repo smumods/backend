4ec55b29593f52d9dbb573f35737b9b110c2a7c3
Gabriel <zhchuan.2016@gmail.com>
2019-08-16 15:20:52 +0800

    Update query type to expose new queries for upcoming events on month

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,8 +1,12 @@
 module Types
   class QueryType < Types::BaseObject
+    # HelloWorld for Debugging 
     field :hello_world, resolver: Queries::HelloWorld
+
+    # Users
     # field :users, resolver: Queries::Users::Users
     field :user, resolver: Queries::Users::User
+
     field :temporary_user, resolver: Queries::TemporaryUsers::TemporaryUser
     field :all_courses, resolver: Queries::Courses::Courses
     field :course, resolver: Queries::Courses::Course
@@ -21,6 +25,8 @@ module Types
     field :all_rsvp_events, resolver: Queries::Events::AllRsvpEvents
     field :event, resolver: Queries::Events::Event
     field :all_recommended_events, resolver: Queries::Events::AllRecommendedEvents
+    field :all_upcoming_events_on_month, resolver: Queries::Events::AllUpcomingEventsOnMonth
+
 
     # Search
     field :search, resolver: Queries::ReviewsSearch
