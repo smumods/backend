ae5ee576648c55aea3533b2f7adac1c9c2a08caa
Gabriel <zhchuan.2016@gmail.com>
2019-08-16 14:22:15 +0800

    Update search to return clubs that belongs to events found in search

--- app/graphql/queries/events_search.rb
+++ app/graphql/queries/events_search.rb
@@ -8,10 +8,26 @@ module Queries
 
         def resolve(query_string:)
             return if query_string.empty?
-            results = []
-            results << ::Club.where("lower(name) ilike ?", "%#{query_string}%").to_a
-            results << ::Event.where("lower(name) ilike ? OR description ilike ?", "%#{query_string}%", "%#{query_string}%").to_a
-            return results.flatten
+            clubs = Set.new
+            events = Set.new
+            # Search clubs by query string
+            clubs_by_query_string = ::Club.where("lower(name) ilike ?", "%#{query_string}%")
+            clubs_by_query_string.each do |club|
+                clubs.add club
+            end
+            # Search events by query string AND by club_id
+            events_by_query_string = ::Event.where("lower(name) ilike ? OR description ilike ? OR club_id in (?)", "%#{query_string}%", "%#{query_string}%", clubs_by_query_string.ids).order('start_date DESC')
+            club_ids_for_events_by_query_string = []
+            events_by_query_string.each do |event|
+                club_ids_for_events_by_query_string |= [event.club_id] # Set operation
+                events.add event
+            end
+            # Requery clubs by club_ids found in events
+            clubs_by_events_query_string = ::Club.where(id: club_ids_for_events_by_query_string)
+            clubs_by_events_query_string.each do |club|
+                clubs.add club
+            end
+            return clubs.to_a + events.to_a
         end
     end
 end
