aed69069bb116d65089db803d5dcdfd21de1e359
Gabriel <zhchuan.2016@gmail.com>
2019-08-16 14:24:40 +0800

    Remove pagination from allUpcomingEvents query

--- app/graphql/queries/events/all_upcoming_events.rb
+++ app/graphql/queries/events/all_upcoming_events.rb
@@ -3,7 +3,7 @@ module Queries
         class AllUpcomingEvents < Queries::BaseQuery
             description 'Returns a Paginable List of Upcoming Events'
 
-            type Types::EventType.connection_type, null: false
+            type [Types::EventType], null: false
 
             def resolve()
                 ::Event.where("start_date >= ? and require_rsvp = false", Time.now).order("start_date ASC")
