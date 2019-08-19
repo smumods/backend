5a2357e6656319d72020f22373ef136a717af1d9
Gabriel <zhchuan.2016@gmail.com>
2019-08-19 17:03:48 +0800

    Add addTelegramLogin mutation to link telegram_id with a currently logged in user

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,6 +1,7 @@
 module Types
   class MutationType < BaseObject
     field :create_user, mutation: Mutations::Users::CreateUser
+    field :add_telegram_login, mutation: Mutations::Users::AddTelegramLogin
     field :create_temporary_user, mutation: Mutations::Users::CreateTemporaryUser
     field :login_user, mutation: Mutations::Users::LogInUser
     field :logout_user, mutation: Mutations::Users::LogOutUser
