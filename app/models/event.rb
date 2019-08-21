53d888e4fcda99ebb1d6ea4c995391b67657bf1c
Gabriel <zhchuan.2016@gmail.com>
2019-08-21 18:42:57 +0800

    Add notifications for club members whenever an event is created

--- app/models/event.rb
+++ app/models/event.rb
@@ -13,4 +13,15 @@ class Event < ApplicationRecord
   validates :location, presence: true
   validates :color, presence: true
   validates :color, inclusion: { in: ["background: rgba(7,64,249,0.70);", "background: rgba(218,27,18,0.70);", "background: rgba(124,157,126,0.70);", "background: rgba(76,80,87,0.70);"] }
+
+  # Callbacks
+  after_create :notify_members_of_event
+
+  private
+  def notify_members_of_event
+    self.club.club_memberships.each do |user|
+      binding.pry
+      NotificationsWorker.perform_async(:notify_members_of_event, user.id, { image: self.main_image, message: "[#{self.club.name}] #{self.name} will be organizing an event on #{self.start_date} at #{self.location}" })
+    end
+  end
 end
--- app/workers/notifications_worker.rb
+++ app/workers/notifications_worker.rb
@@ -2,18 +2,26 @@ class NotificationsWorker
   include Sidekiq::Worker
   sidekiq_options retry: false
 
-  def perform(task, user_id)
+  def perform(task, user_id, options)
     # Map to corresponding task
-    send(task.to_sym, user_id)
+    options ||= {}
+    binding.pry
+    send(task.to_sym, user_id, options)
   end
 
-  def send_welcome_message(user_id)
+  def send_welcome_message(user_id, options)
     user = User.find(user_id)
     Telegram.bot.send_message(chat_id: user.telegram_id, text: "Welcome to SMUMods! You have successfully linked your account with #{user.email}")
   end
 
-  def send_new_review_message(user_id)
+  def send_new_review_message(user_id, options)
     user = User.find(user_id)
     Telegram.bot.send_message(chat_id: user.telegram_id, text: "Hello")
+  end
+
+  def notify_members_of_event(user_id, options)
+    binding.pry
+    user = User.find(user_id)
+    Telegram.bot.send_photo(chat_id: user.telegram_id, photo: options["image"], caption: options["message"])
   end
 end
