8d1413ae06a59d3d9404309b53c7df4416b92ccc
Yash Gadodia <pirsquare.yash@gmail.com>
2019-12-18 14:16:08 +0800

    Add allBooks to UserType

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -9,6 +9,7 @@ module Types
     field :books, [Types::BookType], null: true
     field :books_count, Integer, null: true
     field :all_votes, [Types::VoteType], null: true
+    field :all_books, [Types::BookType], null: true
     field :all_reviews, [Types::ReviewType], null: true
     field :course_bookmarks, [Types::CourseType], null: true
     field :professor_bookmarks, [Types::ProfessorType], null: true
@@ -54,6 +55,10 @@ module Types
 
     def all_votes
       self.object.votes
+    end
+    
+    def all_books
+      self.object.books
     end
 
     def all_reviews
