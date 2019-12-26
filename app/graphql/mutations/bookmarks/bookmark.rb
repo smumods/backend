c7df6fb7c9f5e09f4595b840049d63bb00f9c269
Gabriel <zhchuan.2016@gmail.com>
2019-12-26 23:52:48 +0800

    Add saveBooks mutation

--- app/graphql/mutations/bookmarks/bookmark.rb
+++ app/graphql/mutations/bookmarks/bookmark.rb
@@ -38,7 +38,7 @@ module Mutations
                     end
                 when "book"
                     book = Book.find_by(uuid: resource_id_or_slug)
-                    return if Book.nil?
+                    return if book.nil?
                     if current_user.bookmark_book? book
                         current_user.destroy_action(:bookmark, target: book)
                     else
