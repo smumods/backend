d0c890b182ffb206cfc171566cfd119cf170f755
Gabriel <zhchuan.2016@gmail.com>
2019-12-29 00:09:12 +0800

    Fix user query

--- app/graphql/queries/users/user.rb
+++ app/graphql/queries/users/user.rb
@@ -5,9 +5,10 @@ module Queries
             
             argument :id, String, required: false
 
-            type Types::UserType, null: false
+            type Types::UserType, null: true
 
-            def resolve(id:)
+            def resolve(**args)
+                id = args[:id]
                 return ::User.find_by(uuid: id) if id.present?
                 return context[:current_user] if context[:current_user].present?
                 raise GraphQL::ExecutionError.new("You need a valid User ID or you must be logged in")
