293fc6c14b8a86a7705a6c4498986efd8ece9259
Gabriel <zhchuan.2016@gmail.com>
2019-12-31 17:39:01 +0800

    Fix devise password validation error

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,7 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, 
-         :database_authenticatable, password_length: 8..128
+  devise :database_authenticatable, :registerable, :validatable, password_length: 8..128
 
   # Relationships
   has_many :books, dependent: :destroy
