479bb8e1713f0729bbf576685b7db03a22247019
Gabriel <zhchuan.2016@gmail.com>
2019-12-31 22:18:35 +0800

    Update ActiveAdmin for staging to debug verification

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -7,10 +7,15 @@ ActiveAdmin.register User do
   index do
     selectable_column
     id_column
+    column :uuid
     column :email
     column :first_name
     column :last_name
     column :verified
+    if Rails.env.staging? or Rails.env.development?
+      column :email_token
+      column :verification_count
+    end
     column :password_reset_tries_count
     column :password_token_tries_count
     column :created_at
@@ -32,6 +37,8 @@ ActiveAdmin.register User do
       f.input :last_name, as: :readonly
       if Rails.env.staging? or Rails.env.development?
         f.input :verified
+        f.input :email_token
+        f.input :verification_count
       else
         f.input :verified, as: :readonly
       end
