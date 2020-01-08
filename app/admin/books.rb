29dcf4ee9367f0d7e0c859e3919c08f186fda8b5
Gabriel <zhchuan.2016@gmail.com>
2020-01-08 08:49:12 +0800

    Sort books in admin listings

--- app/admin/books.rb
+++ app/admin/books.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register_page "Books" do
     content do
         render 'listings', { 
-            categories_with_books: Book.joins(:course).includes(:course).where(is_sold: false).group_by { |book| book.course.module_code } 
+            categories_with_books: Book.joins(:course).includes(:course).where(is_sold: false).group_by { |book| book.course.module_code }.sort
         }
     end
 end
