4babb7ce52d2c90b9c4c8d69ee1c35480bb3cc5f
Gabriel <zhchuan.2016@gmail.com>
2020-07-01 18:41:21 +0800

    Remove email and sort by count for leaderboard

--- app/admin/leaderboard.rb
+++ app/admin/leaderboard.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register_page "Leaderboard" do
     content do
         render 'users', { 
-            users: Review.select(:user).group(:user).count,
+            users: Review.select(:user).group(:user).order("count_user DESC").count,
             reviews_count: Review.count,
             books_count: Book.count
         }
