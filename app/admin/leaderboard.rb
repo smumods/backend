9dc48e24805335cca02b875e231f23fdc953ded9
Gabriel <zhchuan.2016@gmail.com>
2020-07-01 23:30:54 +0800

    Add in all users and filtered users (by reviews created within timeframe)

--- app/admin/leaderboard.rb
+++ app/admin/leaderboard.rb
@@ -1,7 +1,8 @@
 ActiveAdmin.register_page "Leaderboard" do
     content do
         render 'users', { 
-            users: Review.select(:user).group(:user).order("count_user DESC").where("reviews.created_at >= ? and reviews.created_at <= ?", "2020/06/15".to_date, "2020/06/28".to_date).count,
+            all_users: Review.select(:user).group(:user).order("count_user DESC").count,
+            filtered_users: Review.select(:user).group(:user).order("count_user DESC").where("reviews.created_at >= ? and reviews.created_at <= ?", "2020/06/15".to_date, "2020/06/28".to_date).count,
             reviews_count: Review.count,
             books_count: Book.count
         }
