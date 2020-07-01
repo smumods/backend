8baafc9ae19328e9b1e627f3c80a5bbfbabf17eb
Yash Gadodia <pirsquare.yash@gmail.com>
2020-07-01 12:51:20 +0800

    marketing dashboard per specs

--- app/admin/leaderboard.rb
+++ app/admin/leaderboard.rb
@@ -1,7 +1,10 @@
 ActiveAdmin.register_page "Leaderboard" do
     content do
         render 'users', { 
-            users: Review.select(:user).group(:user).count
+            users: Review.select(:user).group(:user).count,
+            reviews: Review,
+            books: Book
         }
+
     end
 end
